ffb52aa7a81009f8bb7e5382f23cc60cd70d47c6
majharul224 <majharulislam2225@gmail.com>
2022-02-23 12:15:49 +0600

    first commit

---
+++ map.js
@@ -0,0 +1,29 @@
+/* const numbers=[4,6,8,10,12];
+const output = [];
+for (const number of numbers){
+    const result = number *2;
+    output.push(result);
+}
+console.log(output); */
+
+
+/* 
+const numbers = [4,6,8,10,12]
+const output = [];
+function doubleIt(number){
+    return number*2;
+}
+for( const number of numbers){
+    const result =doubleIt(number);
+    output.push(result);
+}
+console.log(output); */
+/* 
+const numbers = [4,6,8,10,12]
+const output = [];
+const doubleIt = number => number *2; 
+for( const number of numbers){
+    const result =doubleIt(number);
+    output.push(result);
+}
+console.log(output);  */
